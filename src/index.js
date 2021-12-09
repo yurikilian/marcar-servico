409ed54dd9f59a525b7723fbb631ea80d3bb3ddf
Yuri Kilian <kilian.inf@gmail.com>
2021-12-09 15:25:31 +0000

    API-2864: Add sub context path
    Signed-off-by: Yuri Kilian <kilian.inf@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import dotenv from "dotenv";
 import puppeteer from "puppeteer-core";
 import Bot from "./bot.js";
 import { notifyText, notifyImage } from "./telegram.js";
-import log from "./log.js";
 import { CronJob } from "cron";
+import { format } from "date-fns";
 
 dotenv.config();
 
 const { CHROME_PATH } = process.env;
-const MAX_TRIES = 5;
-
-let TRIES = 0;
 
 async function launchBrowser() {
   const browser = await puppeteer.launch({
@@ -25,8 +22,6 @@ async function launchBrowser() {
 }
 
 async function run() {
-  notifyText("Verficando disponibilidade do servico");
-
   const { browser, page } = await launchBrowser();
 
   try {
@@ -35,30 +30,22 @@ async function run() {
     await page.screenshot({ path: "./image.png" });
 
     if (isAvailable) {
-      await notifyImage("Horário disponível");
+      await notifyImage("@yurikilian. Horário disponível na agenda!");
     } else {
       await notifyText("Nenhum Horário disponvível");
     }
-
-    log(isAvailable);
   } catch (err) {
     console.error(err);
-
-    await notifyText(
-      `[WARNING] Falha ao executar o verificador. ${err.message}`
-    );
+    await notifyText(`[ERRO] Falha ao executar o verificador. ${err.message}`);
   } finally {
     await browser.close();
   }
 }
 
 (async () => {
-  console.log("Servico inicializado: ", process.env.CRONTAB);
-
   const options = {
     cronTime: process.env.CRONTAB,
     onTick: async () => {
-      console.log("Execução de serviço disparada");
       await run();
     },
     runOnInit: true,
@@ -66,4 +53,11 @@ async function run() {
 
   const job = new CronJob(options);
   job.start();
+
+  notifyText(
+    `Procurando agenda para @yurikilian. Data de início: ${format(
+      new Date(),
+      "dd/MM/yyyy"
+    )}. ${process.env.MSG_TIME}`
+  );
 })();
